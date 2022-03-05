7fec130915ff0ba9e3bae6432b8df2790feea38f
David Suchan <dav.suchan@gmail.com>
2022-03-05 15:10:19 +0100

    all-components-created-and-router-connected-to-vue-app

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import { createApp } from 'vue';
 
-createApp({}).mount('#app');
+// Importing router and the main app
+import router from './router.js';
+import App from './App.vue';
+
+// Connecting app and router together
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
--- src/router.js
+++ src/router.js
@@ -1,21 +1,29 @@
 // Here, all the routing logic will be stored
 import { createRouter, createWebHistory } from 'vue-router';
 
+// Importing all the components for all the routes
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   router: [
     { path: '/', redirect: '/coaches' }, // If user loads '/' route, then will be redirected to '/coaches' route
-    { path: '/coaches', component: null },
+    { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: null,
+      component: CoachDetail,
       children: [
-        { path: 'contact', component: null }, // /coaches/:id/contact
+        { path: 'contact', component: ContactCoach }, // /coaches/:id/contact
       ],
     },
-    { path: '/register', component: null },
-    { path: '/requests', component: null },
-    { path: '/:notFound(.*)', component: null }, // This will be shown if user is looking for something that doesn't exists
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFound(.*)', component: NotFound }, // This will be shown if user is looking for something that doesn't exists
   ],
 });
 
